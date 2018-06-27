4f737583c554c425846b316ad3b991f478e3a432
canesti22 <40271990+canesti22@users.noreply.github.com>
2018-06-27 09:49:49 +0000

    menu

--- bank_management.c
+++ bank_management.c
@@ -2,6 +2,9 @@
 #include<stdlib.h>
 #include<windows.h>
 
+void menu();
+
+
 void fordelay(int j)
 {   int i,k;
     for(i=0;i<j;i++)
@@ -13,7 +16,38 @@ void close(void)
     printf("\n\n\n\nThis C Project is developed by Bochoris and Anesti!");
 }
 
+void menu(void)
+{
+    int choice;
+    system("cls");
+    system("color 9");
+    printf("\n\n\t\t\tCUSTOMER ACCOUNT BANKING MANAGEMENT SYSTEM");
+    printf("\n\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2\xB2 WELCOME TO THE MAIN MENU \xB2\xB2\xB2\xB2\xB2\xB2\xB2");
+    printf("\n\n\t\t1.Create new account\n\t\t2.Update information of existing account\n\t\t3.For transactions\n\t\t4.Check the details of existing account\n\t\t5.Removing existing account\n\t\t6.View customer's list\n\t\t7.Exit\n\n\n\n\n\t\t Enter your choice:");
+    scanf("%d",&choice);
+    
+    system("cls");
+    switch(choice)
+    {
+            case 1:new_acc();
+            break;
+            case 2:edit();
+            break;
+            case 3:transact();
+            break;
+            case 4:see();
+            break;
+            case 5:erase();
+            break;
+            case 6:view_list();
+            break;
+            case 7:close();
+            break;
+    }
+}
+    
 int main()
+    
 {
     char pass[16], password[16]="anesti_bochoris";
     int i=0;
